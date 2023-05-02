4809c547242288622a2c36fd946d22ca768e28eb
unknown <Apoorv-Bhatnagar>
2023-05-02 15:17:27 +0530

    added circuit breaker using resilience4j

--- UserService/UserService/src/main/java/com/apoorv/user/service/UserService/controller/UserController.java
+++ UserService/UserService/src/main/java/com/apoorv/user/service/UserService/controller/UserController.java
@@ -2,7 +2,9 @@ package com.apoorv.user.service.UserService.controller;
 
 import com.apoorv.user.service.UserService.entities.User;
 import com.apoorv.user.service.UserService.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,10 +23,17 @@ public class UserController {
            return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
     @GetMapping("/{userId}")
+    @CircuitBreaker(name="ratinghotelbreaker",fallbackMethod = "ratinghotelbreakerfallback")
     public ResponseEntity<User> getsingleUser(@PathVariable String userId)
     {
         User user=userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    public ResponseEntity<User> ratinghotelbreakerfallback(String userId,Exception ex)
+    {
+        System.out.println("either rating or hotel service is down");
+        return new ResponseEntity<>(new User(), HttpStatus.NOT_FOUND);
     }
     @GetMapping
     public ResponseEntity<List<User>> getallUser()
